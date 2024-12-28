673cb7e7912da223c5a4f5b90e6ad7a642544bb2
dorhu <dorhu@wix.com>
2024-12-28 21:30:11 +0200

    add client code

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
+import { Navigation } from './components/Navigation';
+
+class App extends React.Component {
+  public render(): React.ReactNode {
+    return (
+      <div>
+        <h1>React with TypeScript - 2</h1>
+        <Navigation />
+      </div>
+    );
+  }
+}
+
+const app: React.ComponentType = hot(module)(App);
+
+export { app as App };
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,9 @@
+/**
+ *
+ */
+
+import * as React from 'react';
+
+const Home: React.SFC = (): React.ReactElement<{}> => <h1>Home</h1>;
+
+export { Home };
