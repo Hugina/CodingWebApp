cfa8b8d3089fb84527ca59c6c32bdb97ec27ac4d
Hugina <nadavhugi@gmail.com>
2025-01-02 17:06:33 +0200

    added alert and close button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,20 +10,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Alert from './components/Alert';
 import { useState } from 'react'
 
-class App extends React.Component {
-  public render(): React.ReactNode {
-    const [alertVisible, setAlertVisibility] = useState(false)
-    return (
-      <div>
-        {alertVisible && <Alert>My Alert</Alert>}
-        <h1>React with TypeScript - 2</h1>
-        <Navigation />
-        <Button onClick= {() => setAlertVisibility(true)}>Nadav's Button</Button>
-      </div>
-    );
-  }
+
+const App = () => {
+  const [alertVisible, setAlertVisibility] = useState(false)
+  console.log("APP: alertVisible is: ", alertVisible)
+  return (
+    <div>
+      {alertVisible && <Alert onClose={()=>setAlertVisibility(false)}>My Alert</Alert>}
+      <h1>React with TypeScript - 2</h1>
+      <Navigation />
+      <Button onClick= {() => setAlertVisibility(true)}>Nadav's Button</Button>
+    </div>
+  );
 }
 
-const app: React.ComponentType = hot(module)(App);
 
-export { app as App };
+// const app: React.ComponentType = hot(module)(App);
+
+export default App;
