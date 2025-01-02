c19d29ad7d3b7f27fd8c5b2bfe90f115a980225a
Hugina <nadavhugi@gmail.com>
2025-01-02 14:16:29 +0200

    Added PB

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {
+    children: string
+    onClick: () => void; //what is it and why does it have a semicolon at the end? is this attribute a function?
+    //first class citzen
+    }
+
+const Button = (props:Props) => {
+  return (
+    <button className= 'btn btn-primary' onClick={props.onClick}> {props.children}</button>
+  )
+}
+
+export default Button
